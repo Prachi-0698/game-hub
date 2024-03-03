efc0d4bb8ec8cd672ce62dadf75d6bb07f239be8
Prachi-0698 <prachikumari319@gmail.com>
2024-03-03 23:38:37 +0530

    Added Shimmer UI

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -26,22 +26,29 @@ const useGames = () =>{
 
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState('');
+    const [isLoading, setIsLoading] = useState(false)
     // console.log(games);
     
-
+    
     useEffect(() =>{
     const controller = new AbortController();
+        setIsLoading(true)
         apiClient
          .get<FetchGameResponse>('/games', {signal: controller.signal})
-         .then(res => setGames(res?.data?.results))
+         .then(res => {
+            setGames(res?.data?.results);
+            setIsLoading(false)
+        })
          .catch(err => {
             if(err instanceof CanceledError) return;
-            setError(err.message)})
+            setError(err.message)
+            setIsLoading(false)
+        })
 
          return () => controller.abort()
     }, [])
 
-    return {games, error}
+    return {games, error, isLoading}
 
 }
 
