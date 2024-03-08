0da2e4b60802e74b70028578fa6862d407e208ac
Prachi-0698 <prachikumari319@gmail.com>
2024-03-08 20:47:21 +0530

    Add dynamic heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from '@chakra-ui/react'
+import { Box, Grid, GridItem, HStack, Show } from '@chakra-ui/react'
 import { useState } from "react"
 import Navbar from './components/Navbar'
 import GameGrid from './components/GameGrid'
@@ -7,6 +7,7 @@ import { Genre } from './hooks/useGenre'
 import PlatformSelector from './components/PlatformSelector'
 import { Platform } from './hooks/useGames'
 import SortSelector from './components/SortSelector'
+import GameHeading from './components/GameHeading'
 
 export interface GameQuery{
   genre: Genre | null;
@@ -34,10 +35,14 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area={'main'} >
-        <HStack spacing={5} paddingLeft={2} marginBottom={5}>
-          <PlatformSelector selectedPlatform={gameQuery.platform} onSelectedPlatform={(platform) => setGameQuery({ ...gameQuery ,platform})}/>
-          <SortSelector sortSelector={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})} />
-        </HStack>
+        <Box paddingLeft={2}>
+          <GameHeading gameQuery={gameQuery} />
+          <HStack spacing={5} marginBottom={5}>
+            <PlatformSelector selectedPlatform={gameQuery.platform} onSelectedPlatform={(platform) => setGameQuery({ ...gameQuery ,platform})}/>
+            <SortSelector sortSelector={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})} />
+          </HStack>
+        </Box>
+        
         <GameGrid gameQuery={gameQuery}/>
       </GridItem>
     </Grid>
