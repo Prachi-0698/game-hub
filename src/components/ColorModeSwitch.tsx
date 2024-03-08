62976c0004205a7e458e37f7c4771c9ba168b63e
Prachi-0698 <prachikumari319@gmail.com>
2024-03-08 21:44:57 +0530

    Customize dark mode theme

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,6 +1,6 @@
 import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react"
 
-const ColorModeSwitch = () =>{
+const ColorModeSwitch = () => {
     const {toggleColorMode, colorMode} = useColorMode();
     return (
         <HStack>
